8d52d4d058fb6596956a58e02ce13f56d361a3a3
Thiago Trabuco <163602179+DevThiagoTrabuco@users.noreply.github.com>
2024-04-11 20:21:38 -0300

    Update Exercicio Struct 02 - 01.cpp

--- C/Exercicio Struct 2/Exercicio Struct 02 - 01.cpp
+++ C/Exercicio Struct 2/Exercicio Struct 02 - 01.cpp
@@ -23,6 +23,91 @@ typedef struct {
 UserList users[MAX_USER];
 int op, i = 0;
 
+bool emptyList(){
+	if(i == 0){
+		return true;
+	}
+	return false;
+}
+
+bool fullList(){
+	if (i == MAX_USER){
+		return true;
+	}
+	return false;
+}
+
+void userRegistration(){
+	if(fullList()){
+		system("cls");
+		printf("\t###LISTA DE CADASTROS CHEIA###\n\n");
+		system("pause");
+		system("cls");
+	} else {
+		system("cls");
+		printf("Nome: ");
+		gets(users[i].userName);
+		printf("Idade: ");
+		scanf(" %d", &users[i].userAge);
+		if (users[i].userAge < 18){
+			printf("Cadastro apenas para maiores de 18 anos.\n");
+			system("pause");
+			system("cls");
+		} 
+		else {
+			printf("Telefone (Ex: 912345678): ");
+			scanf(" %d", &users[i].userPhone);
+			fflush(stdin);
+			printf("Email: ");
+			gets(users[i].userEmail);
+			fflush(stdin);
+			do{
+				printf("Usuário: ");
+				gets(users[i].userUsername);
+				if (strlen(users[i].userUsername) < 8){
+					printf("Minímo de 8 carateres para usuário.\n");
+					system("pause");
+					system("cls");
+				}
+			} while (strlen(users[i].userUsername) < 8);
+			do{
+				printf("Senha: ");
+				gets(users[i].userPass);
+				if (strlen(users[i].userPass) < 8){
+					printf("Minímo de 8 carateres para senha.\n");
+					system("pause");
+					system("cls");
+				}
+			} while (strlen(users[i].userPass) < 8);
+		i++;
+		system("pause");
+		system("cls");
+		}
+	}
+}
+
+void userList(){
+	if(emptyList()){
+		system("cls");	
+			printf("\n\t###LISTA VAZIA###\n\n");
+			system("pause"); 
+			system("cls");
+		} else {
+		system("cls");
+		for(int x = 0;x < i;x++){
+			printf("%s", users[x].userName);
+			printf("\n%d", users[x].userAge);
+			printf("\n%d", users[x].userPhone);
+			printf("\n%s", users[x].userEmail);
+			printf("\n%s", users[x].userUsername);
+			printf("\n%s", users[x].userPass);
+			printf("\n\n");
+		}
+		system("pause");
+		system("cls");
+	}
+}
+
 	int main(){
 		setlocale(LC_ALL, "portuguese");
 		while(op != 3){
@@ -35,57 +120,10 @@ int op, i = 0;
 			fflush(stdin);
 			switch(op){
 				case 1:
-					system("cls");
-					printf("Nome: ");
-					gets(users[i].userName);
-					printf("Idade: ");
-					scanf(" %d", &users[i].userAge);
-					if (users[i].userAge < 18){
-						printf("Cadastro apenas para maiores de 18 anos.\n");
-						system("pause");
-						system("cls");
-					} 
-					else {
-						printf("Telefone (Ex: 912345678): ");
-						scanf(" %d", &users[i].userPhone);
-						fflush(stdin);
-						printf("Email: ");
-						gets(users[i].userEmail);
-						fflush(stdin);
-						do{
-							printf("Usuário: ");
-							gets(users[i].userUsername);
-							if (strlen(users[i].userUsername) < 8){
-								printf("Minímo de 8 carateres para usuário.\n");
-								system("pause");
-								system("cls");
-							}
-						} while (strlen(users[i].userUsername) < 8);
-						do{
-							printf("Senha: ");
-							gets(users[i].userPass);
-							if (strlen(users[i].userPass) < 8){
-								printf("Minímo de 8 carateres para senha.\n");
-								system("pause");
-								system("cls");
-							}
-						} while (strlen(users[i].userPass) < 8);
-					i++;
-					system("pause");
-					system("cls");
-					}
+					userRegistration();
 					break;
 				case 2:
-					system("cls");
-					for(int x = 0;x < i;x++){
-						printf("%s", users[x].userName);
-						printf("\n%d", users[x].userAge);
-						printf("\n%d", users[x].userPhone);
-						printf("\n%s", users[x].userEmail);
-						printf("\n%s", users[x].userUsername);
-						printf("\n%s", users[x].userPass);
-						printf("\n\n");
-					}
+					userList();
 					break;
 				case 3:
 					abort();
