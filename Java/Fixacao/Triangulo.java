171888091b9c80341706184aed01d866fa1dff2e
Thiago Trabuco <163602179+DevThiagoTrabuco@users.noreply.github.com>
2024-10-30 19:56:35 -0300

    update

--- Java/Fixacao/Triangulo.java
+++ Java/Fixacao/Triangulo.java
@@ -1,5 +1,20 @@
 package Fixacao;
 
-public class Triangulo {
+public class Triangulo implements ObjGeometrico{
+    private double sideA, sideB, sideC;
+    @SuppressWarnings("FieldMayBeFinal")
+    private double s = (sideA + sideB + sideC)/2;
+
+    @Override
+    public double area() {
+        double area = Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+        return area;
+    }
+
+    @Override
+    public double perimetro() {
+        double perimetro = sideA + sideB + sideC;
+        return perimetro;
+    }
     
 }
