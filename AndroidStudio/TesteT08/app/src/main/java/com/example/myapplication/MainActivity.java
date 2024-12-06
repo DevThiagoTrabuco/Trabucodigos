200a60bb4e4b67450e1cce3145dfe50a468c7e74
Thiago Trabuco <163602179+DevThiagoTrabuco@users.noreply.github.com>
2024-12-06 20:50:56 -0300

    Update

--- AndroidStudio/TesteT08/app/src/main/java/com/example/myapplication/MainActivity.java
+++ AndroidStudio/TesteT08/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,11 +42,75 @@ public class MainActivity extends AppCompatActivity {
 
         email = findViewById(R.id.mail);
         senha = findViewById(R.id.senha);
-        pb = findViewById(R.id.progressBar2);
+        pb = findViewById(R.id.progressBar);
         cadastro = findViewById(R.id.cadastro);
     }
 
-    public void cadastro(View v){
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        FirebaseUser userAtual = FirebaseAuth.getInstance().getCurrentUser();
+        if(userAtual != null){
+            chamarPerfil();
+        }
+    }
+
+    public void login(View v){
+        String mail = email.getText().toString();
+        String pass = senha.getText().toString();
+
+        if(mail.isEmpty() || pass.isEmpty()){
+            Snackbar snac = Snackbar.make
+                    (v, "Preencha todos os campos", Snackbar.LENGTH_SHORT);
+            snac.setBackgroundTint(Color.WHITE);
+            snac.setTextColor(Color.BLACK);
+            snac.show();
+        } else {
+            loginUser(v);
+        }
+    }
+
+    private void loginUser(View v) {
+        String mail = email.getText().toString();
+        String pass = senha.getText().toString();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(mail, pass).
+                addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    pb.setVisibility(View.VISIBLE);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            chamarPerfil();
+                        }
+                    }, 3000);
+                } else {
+                    String erro;
+                    try{
+                        throw task.getException();
+                    } catch(Exception e){
+                        erro = "Email ou senha incorretos";
+                    }
+                    Snackbar snac = Snackbar.make
+                            (v, erro, Snackbar.LENGTH_SHORT);
+                    snac.setBackgroundTint(Color.WHITE);
+                    snac.setTextColor(Color.BLACK);
+                    snac.show();
+                }
+            }
+        });
+    }
+
+    private void chamarPerfil() {
+        Intent i = new Intent(getApplicationContext(), MainActivity3.class);
+        startActivity(i);
+    }
+
+    public void chamarCadastro(View v){
         Intent i = new Intent(getApplicationContext(), MainActivity2.class);
         startActivity(i);
     }
--- AndroidStudio/TesteT08/app/src/main/java/com/example/myapplication/MainActivity3.java
+++ AndroidStudio/TesteT08/app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -1,14 +1,28 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
 public class MainActivity3 extends AppCompatActivity {
+
+    private TextView nome, email;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,32 @@ public class MainActivity3 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        nome = findViewById(R.id.txtNome);
+        email = findViewById(R.id.txtEmail);
+    }
+
+    public void onStart(){
+        super.onStart();
+
+        String mail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference dr = db.collection("Usuarios"). document(userId);
+        dr.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                if(value != null){
+                    nome.setText(value.getString("Nome"));
+                    email.setText(mail);
+                }
+            }
+        });
+    }
+
+    public void logoff(View v){
+        FirebaseAuth.getInstance().signOut();
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
